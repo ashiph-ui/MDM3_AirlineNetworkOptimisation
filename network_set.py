08dd2d2405ee508677660b375f6e08562b39db85
Lisa <op20927@bristol.ac.uk>
2023-03-13 22:03:33 +0000

    modified:   network_set.py 	modified:   output.csv

--- network_set.py
+++ network_set.py
@@ -3,15 +3,26 @@ from datetime import datetime
 import pandas as pd
 import airportsdata
 import requests
+import csv
 
 flightstack_key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiNThlNzY3NTNkZjQ4Y2I5MzViOGQ3MzQzYzQwMmFmYjNlM2U5M2NmM2NjNGE4MGE4NjNlY2UxMzU5NTM4MTUwOTMyMmI4Nzk5ZTVkODg0MjQiLCJpYXQiOjE2Nzg3MjA1OTEsIm5iZiI6MTY3ODcyMDU5MSwiZXhwIjoxNzEwMzQyOTkxLCJzdWIiOiIyMDQ0OSIsInNjb3BlcyI6W119.QSW0Qh663z_nRnEWK8ocJbVwBq-SW7tlQSxUwRcVdrJYO0UmOQqU_YKaPFXkmJ3m2sHckQ7xKW5Jfw_in9SmoQ"
-airports = airportsdata.load('IATA')
+airports_icao = airportsdata.load('ICAO')
+airports_iata = airportsdata.load('IATA')
 # datetime object containing current date and time
 now = datetime.now()
 
 # easyjet = Flights(airline="EZY", airport="all")
 # easyjet.df.to_csv(now.strftime("%Y-%m-%d_%H-%M-%S") + "_EZY.csv")
 df = pd.read_csv("2023-03-13_13-29-44_EZY.csv")
+
+airport_df = pd.read_csv("output.csv")
+airport_missing_list = ["EGPE EGPD EGAA EGAC LSZH LCLK EDDM EDNY LYBE LEST EGNM LEAM LEMI LEBB LFBZ LFMT LFML LFTH LFLS LFRB LFOB EGSS EGHL EGHI LGSR LGIR LGRP LIME LIPX LIRP LIPE LIBD LIBR LICC GMAD LPPS ESSA EFRO LTBS LTAI OJAQ HESH HEGN HEMA"]
+airport_missing_list = airport_missing_list[0].split(" ")
+
+
+values = []
+for airport in airport_missing_list:
+    values.append(list(airports_icao[airport].values()))
 
 # creating dataset of nodes and edges for network graph
 nodes = []
@@ -24,18 +35,11 @@ for flight in df.origin_airport_iata.unique():
 for flight in df.itertuples():
     edges.append((flight.origin_airport_iata, flight.destination_airport_iata))
 
-# creating network graph
-header.append(airports['LGW'].keys())
-print(str(header))
-import csv
-values = []
-keys = list(airports['LGW'].keys())
+values_2 = []
+keys = list(airports_icao['EGKK'].keys())
 #create dict of data
 for airport in nodes:
-    
-
-    values.append(list(airports[airport].values()))
-    
+    values.append(list(airports_iata[airport].values()))
 
 
 # open a CSV file for writing
@@ -47,20 +51,19 @@ with open('output.csv', mode='w', newline='\n') as file:
     # write the header row
     writer.writerow(keys)
 
+    for value_2 in values_2:
+        writer.writerow(value_2)
+
     # write the data row
     for value in values:    
         writer.writerow(value)
 
 
-
-# with open("network_set.csv", "w") as f:
-#     f.write(f"{item}")
-
-params = {
-  'access_key': flightstack_key,
-  'airline_iata': 'EZY',
-  'iataCode': 'LGW'
-}
+# params = {
+#   'access_key': flightstack_key,
+#   'airline_iata': 'EZY',
+#   'iataCode': 'LGW'
+# }
 
 # api_result = requests.get('https://app.goflightlabs.com/flights', params)
 
