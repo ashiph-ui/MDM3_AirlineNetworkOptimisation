cc313b883247eaf07106df96bb941d1f64ef7c49
Lisa Devillers <op20927@bristol.ac.uk>
2023-03-30 11:54:45 +0200

    delay file not working idk why

--- network_opt.py
+++ network_opt.py
@@ -1,3 +1,44 @@
 from make_network import *
 from ortools.constraint_solver import pywrapcp, routing_enums_pb2
 
+def generate_schedule(routes):
+    # Define the time span for the schedule
+    start_time = datetime.datetime(2023, 4, 1, 8, 0, 0) # April 1st, 2023, 8:00am
+    end_time = datetime.datetime(2023, 4, 7, 22, 0, 0) # April 7th, 2023, 10:00pm
+
+    # Define the time interval between flights
+    time_interval = datetime.timedelta(hours=2)
+
+    # Initialize the schedule as an empty list
+    schedule = []
+
+    # Loop through each route and generate flights for the schedule
+    for route in routes:
+        # Get the departure and arrival airports
+        departure_airport = route[0]
+        arrival_airport = route[-1]
+
+        # Determine the number of flights to schedule based on demand
+        num_flights = determine_num_flights(departure_airport, arrival_airport)
+
+        # Determine the departure time for each flight
+        current_time = start_time
+        for i in range(num_flights):
+            # Create a new flight for the current time
+            flight = {
+                "departure_airport": departure_airport,
+                "arrival_airport": arrival_airport,
+                "departure_time": current_time,
+                "arrival_time": current_time + calculate_travel_time(route),
+                "aircraft_type": determine_aircraft_type(route)
+            }
+            schedule.append(flight)
+
+            # Increment the current time by the time interval
+            current_time += time_interval
+
+            # If the current time exceeds the end time, stop scheduling flights
+            if current_time > end_time:
+                break
+
+    return schedule
