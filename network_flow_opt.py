ef07d9f2b231e91a8cc41231ed89f43f29af927d
Lisa Devillers <op20927@bristol.ac.uk>
2023-04-06 13:05:27 +0200

    added comments

--- network_flow_opt.py
+++ network_flow_opt.py
@@ -5,9 +5,11 @@ import numpy as np
 # Load nodes and edges from csv file
 airports_df = pd.read_csv('node_eurocontrol_w_bases.csv', encoding='cp1252', index_col='index')
 flights_df = pd.read_csv('edge_eurocontrol_minute.csv', encoding='cp1252')
-# Create a graph with nodes for each airport and arcs for each flight
+# append number of nodes and flights (arcs)
 num_nodes = len(airports_df)
 num_arcs = len(flights_df)
+
+# call the min cost flow solver 
 smcf = min_cost_flow.SimpleMinCostFlow()
 
 src = []
@@ -21,18 +23,17 @@ for i in range(num_arcs):
     dst.append(airports_df[airports_df['icao']==flights_df.iloc[i]['destination_airport_icao']].index.values[0])
     cost.append(flights_df.iloc[i]['co2'])
    
-
+# Add constraint and capacity for each arc.
 all_arcs = smcf.add_arcs_with_capacity_and_unit_cost(src, dst, capacity, cost)
 
 # Define the flow conservation constraints for each airport
-# Add supply for each nodes.
+# Add supply for each nodes. (not yet implemented)
 smcf.set_nodes_supplies(np.arange(0, len(supplies)), supplies)
 # Define any additional constraints, such as minimum or maximum delay times
 status = smcf.solve()
-# Solve the problem and print the optimal solution
 
+# Solve the problem and print the optimal solution
 if status != smcf.OPTIMAL:
-        
         print('There was an issue with the min cost flow input.')
         print(f'Status: {status}')
         exit(1)
