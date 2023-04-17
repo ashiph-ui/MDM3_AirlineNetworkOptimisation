a9cb3a6ad3b4c7658d60bd3d866cec1285d471eb
Quincy <68078970+qcuincy@users.noreply.github.com>
2023-04-17 18:53:56 +0100

    Update __init__.py

--- realtime/__init__.py
+++ realtime/__init__.py
@@ -2,3 +2,7 @@ from .core import *
 from .request import *
 from .flight import *
 from .api import *
+from .flights import *
+from .flightschedule import *
+from .flightschedules import *
+from .simulatedannealing import *
